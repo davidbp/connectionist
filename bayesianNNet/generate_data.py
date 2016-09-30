9d23fa10ae61190f80add1e51212cee39fd76056
David Buchaca <davidbuchaca@gmail.com>
2016-09-30 18:31:30 +0200

    bayesian material added

--- bayesianNNet/generate_data.py
+++ bayesianNNet/generate_data.py
@@ -22,3 +22,48 @@ def generate_2d_data(num_samples=500, seed_val = 123):
     X = np.vstack((X1, X2)).T
 
     return X, np.array(Y)
+
+
+def generate_1dlinear_data(num_samples=500, seed_val = 123): 
+    # Initialize random number generator
+    np.random.seed(seed_val)
+    
+    # True parameter values
+    alpha, sigma = 1, 1
+    beta = 1
+    
+    # Size of dataset
+    dataset_size = 500
+    size = dataset_size
+    
+    # Predictor variable
+    X = np.random.randn(size)
+    
+    # Simulate outcome variable
+    Y = alpha + beta*X + np.random.randn(size)*sigma
+
+
+    return X, np.array(Y)
+
+
+
+
+
+def generate_1dsinusoidal_data(num_samples=500, seed_val = 123): 
+    # Initialize random number generator
+    np.random.seed(seed_val)
+    
+    # True parameter values
+    sigma = 1, 1
+    
+    # Size of dataset
+    dataset_size = 500
+    size = dataset_size
+    
+    # Predictor variable
+    X = np.random.uniform(-5, 5, size)
+    
+    # Simulate outcome variable
+    Y = np.sin(X)
+
+    return X, np.array(Y)
