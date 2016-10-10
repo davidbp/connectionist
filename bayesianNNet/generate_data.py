185f7f29251d53b0b3b077bf98433811c47451ad
David Buchaca <davidbuchaca@gmail.com>
2016-10-10 17:30:48 +0200

    adding mixture_density_network stuff

--- bayesianNNet/generate_data.py
+++ bayesianNNet/generate_data.py
@@ -48,8 +48,10 @@ def generate_1dlinear_data(num_samples=500, seed_val = 123):
 
 
 
-
-def generate_1dsinusoidal_data(num_samples=500, seed_val = 123): 
+def generate_1dsinusoidal_data(x_min = -5,
+                               x_max = 10,
+                               num_samples=500,
+                               seed_val = 123): 
     # Initialize random number generator
     np.random.seed(seed_val)
     
@@ -57,13 +59,28 @@ def generate_1dsinusoidal_data(num_samples=500, seed_val = 123):
     sigma = 1, 1
     
     # Size of dataset
-    dataset_size = 500
-    size = dataset_size
+    size = num_samples
     
     # Predictor variable
-    X = np.random.uniform(-5, 5, size)
+    X = np.random.uniform(x_min, x_max, size)
+    
+    ind_left = X< -1
+    ind_right = X>1
+    # ind middle is  -1<=X<=1
+    ind_middle = ~ind_left * ~ind_right
+    
+    # noise
+    Ynoise_small = np.random.normal(0, 0.2, ind_left.sum())
+    Ynoise_verysmall =  np.random.normal(0, 0.05, ind_middle.sum())
+    Ynoise_big = np.random.normal(0, 0.5, ind_right.sum())
     
     # Simulate outcome variable
     Y = np.sin(X)
-
+    Y[ind_left] = Y[ind_left] + Ynoise_small
+    Y[ind_middle] = Y[ind_middle] + Ynoise_verysmall
+    Y[ind_right] =  Y[ind_right] + Ynoise_big
+    
     return X, np.array(Y)
+
+
+
